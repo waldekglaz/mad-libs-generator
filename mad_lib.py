b995cef2dda987d7542e481b5094812e3d2e6fe7
waldekglaz <waldekglaz@gmail.com>
2021-08-31 20:51:56 +0100

    if statement

--- mad_lib.py
+++ mad_lib.py
@@ -2,19 +2,10 @@
 import pyinputplus as pyip
 
 
-start_new_game = True
-
-# start the game yes or no
 new_game = pyip.inputYesNo("Do you want to start new game? yes or no :")
 
-if new_game == "yes":
+if new_game:
     print("Welcome!")
-    start_new_game = True
-elif new_game == "no":
-    start_new_game = False
-    print("Good Bye!")
-
-while start_new_game == True:
     name = pyip.inputStr("Please enter a name: ")
     silly_word = pyip.inputStr("Please enter a silly word: ")
     number = pyip.inputNum("Please enter a number: ")
@@ -28,4 +19,34 @@ while start_new_game == True:
     adjective_5 = pyip.inputStr("Please enter the last one adjective: ")
 
     print(f"Hello my name is astronaut {name}. I am on my way to planet {silly_word}.I will be gone for {number} days. I am very {adjective} about the trip but I will miss my {noun}. I have heard that the atmosphere there is {adjective_2}. Luckily {relative} packed me a jacket to keep me {adjective_3}. When I land on the planet I will {verb} for joy. I am {adjective_4} to walk on another planet. I could not be more {adjective_5} for this trip!")
-    break
+
+elif new_game == "no":
+    print("Good Bye!")
+
+# start_new_game = True
+
+# # start the game yes or no
+# new_game = pyip.inputYesNo("Do you want to start new game? yes or no :")
+
+# if new_game == "yes":
+#     print("Welcome!")
+#     start_new_game = True
+# elif new_game == "no":
+#     start_new_game = False
+#     print("Good Bye!")
+
+# while start_new_game == True:
+#     name = pyip.inputStr("Please enter a name: ")
+#     silly_word = pyip.inputStr("Please enter a silly word: ")
+#     number = pyip.inputNum("Please enter a number: ")
+#     adjective = pyip.inputStr("Please enter first adjective: ")
+#     noun = pyip.inputStr("Please enter a noun: ")
+#     adjective_2 = pyip.inputStr("Please enter another adjective: ")
+#     relative = pyip.inputStr("Please enter your relative: ")
+#     adjective_3 = pyip.inputStr("Please enter another adjective: ")
+#     verb = pyip.inputStr("Please enter a verb: ")
+#     adjective_4 = pyip.inputStr("Please enter another adjective: ")
+#     adjective_5 = pyip.inputStr("Please enter the last one adjective: ")
+
+#
+#     break
